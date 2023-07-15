b465db546c16daeb82af35666353f6081044e5e6
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2023-07-14 20:00:51 -0400

    initial commit

---
+++ src/GradeBook/Notes/TestingNotes.cs
@@ -0,0 +1,11 @@
+
+/*
+        # Unit Testing
+            is automated
+            investigate 
+            small Units of Code
+            Test Runner
+            Automation
+
+        Test Library.  xUnit.net
+*/
---
+++ src/GradeBook/Notes/notes.cs
@@ -0,0 +1,11 @@
+
+/**
+    #1 How to run css file ?
+        in your terminal navigate to your project folder and type: dotnet run
+        if you want pass a parameter type: donet run --parameter_argument
+
+    
+    #2 What degugger does?
+        A debugger can allow me to execute my program in a controlled environment
+        and inspect the environment as I go allong to try to find the source of the problem
+*/
